416cacc41a35948b82581e5f53456dc034e6aa7e
Pit Bul <Nazim55555@yandex.ru>
2024-06-23 19:38:31 +0300

    5. Конвертер валюты [#41583 #501892]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,11 +1,9 @@
 package ru.job4j.converter;
 
 public class Converter {
-
     public static float rubleToEuro(float value) {
         float result = value / 70;
         return result;
-
     }
 
     public static float rubleToDollar(float value) {
