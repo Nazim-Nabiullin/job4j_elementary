91b589bb6998a8a968308ee64b026c0a5d500544
Pit Bul <Nazim55555@yandex.ru>
2024-06-26 09:15:18 +0300

    1.2. Входные данные в тесте [#297750 #502386]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class X2Test {
-
     @Test
     void whenA10B0C0X2Then40() {
         int a = 10;
@@ -17,7 +16,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C1X1Then40() {
+    void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -28,7 +27,7 @@ class X2Test {
     }
 
     @Test
-    void whenA0B1C1X1Then40() {
+    void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -39,7 +38,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C0X1Then40() {
+    void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -50,7 +49,7 @@ class X2Test {
     }
 
     @Test
-    void whenA1B1C1X0Then40() {
+    void whenA1B1C1X0Then0() {
         int a = 1;
         int b = 1;
         int c = 1;
