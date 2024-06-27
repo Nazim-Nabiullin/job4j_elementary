b2f0b7fd778fd1db795679f90e585e955562b178
Pit Bul <Nazim55555@yandex.ru>
2024-06-27 13:29:32 +0300

    GPT. Нейросети спешат на помощь. [#505156 #502571]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,7 +2,6 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    // Константы для расчетов
     private static final int MAN_BASE_HEIGHT = 100;
     private static final int WOMAN_BASE_HEIGHT = 110;
     private static final double WEIGHT_MULTIPLIER = 1.15;
@@ -26,13 +25,11 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187; // Заданный рост
+        short height = 187;
 
-        // Расчет веса для мужчин
         double manWeight = Fit.manWeight(height);
         System.out.printf("Man 187 cm has ideal weight of %.2f kg%n", manWeight);
 
-        // Расчет веса для женщин
         double womanWeight = Fit.womanWeight(height);
         System.out.printf("Woman 187 cm has ideal weight of %.2f kg%n", womanWeight);
     }
