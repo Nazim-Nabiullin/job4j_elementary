b83b250d09563ca58c024c6165f3afe026472b64
Pit Bul <Nazim55555@yandex.ru>
2024-06-27 17:49:38 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyDot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+public class DummyDot {
+    public static String answer(String question) {
+        String result = "I don't know. Please, ask another question";
+        if ("Hi, Bot.".equals(question)) {
+            result = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            result = "See you later.";
+        } else {
+            result = "I don't know, Please, ask another question.";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyDot.answer(("Hi, Bot."));
+        System.out.println(result);
+        result = DummyDot.answer("Bye.");
+        System.out.println(result);
+        result = DummyDot.answer("Hi");
+        System.out.println(result);
+    }
+}
