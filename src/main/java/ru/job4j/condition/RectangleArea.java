2a18ee4e9d0630be4d53414b4b866155a1839efc
Pit Bul <Nazim55555@yandex.ru>
2024-06-28 12:37:10 +0300

    4.5. Отладка программы в IDEA [#159464]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -4,7 +4,8 @@ public class RectangleArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        return h * l;
+        double s = h * l;
+        return s;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/condition/RectangleAreaTest.java
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -15,20 +15,11 @@ class RectangleAreaTest {
     }
 
     @Test
-    void whenP7K5Square1dot70139() {
-        double expected = 1.70139;
-        int p = 7;
-        double k = 5;
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        double k = 1;
         double output = RectangleArea.square(p, k);
-        assertThat(output).isEqualTo(expected, withPrecision(0.00001));
-    }
-
-    @Test
-    void whenPK3Square1dot1718() {
-        double expected = 1.1718;
-        int p = 5;
-        double k = 3;
-        double output = RectangleArea.square(p, k);
-        assertThat(output).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(output).isCloseTo(expected, offset(0.01));
     }
 }
