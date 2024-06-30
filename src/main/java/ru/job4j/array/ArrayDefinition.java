355b8f4374f39024bb7c46a0cc8935e1dffccef8
Pit Bul <Nazim55555@yandex.ru>
2024-06-30 10:04:09 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Один";
+        names[1] = "Два";
+        names[2] = "Три";
+        names[3] = "Четыре";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
