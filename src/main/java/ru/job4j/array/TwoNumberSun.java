de8bfa47cd58f1c9f317a888c5e2fd421031eb3c
Pit Bul <Nazim55555@yandex.ru>
2024-07-05 11:00:19 +0300

    6.8. Метод двух указателей [#225 #503824]

--- src/main/java/ru/job4j/array/TwoNumberSun.java
+++ src/main/java/ru/job4j/array/TwoNumberSun.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class TwoNumberSun {
     public static int[] getIndexes(int[] array, int target) {
-        Arrays.sort(array);
         int left = 0;
         int right = array.length - 1;
         while (left < right) {
